8df9e9c8333879f15ffb5c17ec93ca9652e6ce00
Fatih Emre Salman <fatih.fes.538@gmail.com>
2023-01-15 12:07:08 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <ChildComponent :title="title" @titleChanging="fatih($event)" />
+</template>
+
+<script>
+import ChildComponent from "./components/ChildComponent.vue";
+export default {
+  name: "App",
+  components: {
+    ChildComponent,
+  },
+  data() {
+    return {
+      title: "Hello",
+    };
+  },
+  methods: {
+    fatih(newTitle) {
+      this.title = newTitle;
+    },
+  },
+};
+</script>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" v-model="newTitle" />
+  <button @click="changeTitle()">Change title</button>
+</template>
+
+<script>
+export default {
+  name: "ChildComponent",
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      newTitle: "",
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.$emit("titleChanging", this.newTitle);
+    },
+  },
+};
+</script>
